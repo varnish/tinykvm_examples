34b353cf3d1bd106c8a129d59ae184289cb4d275
Laurence Rowe <l@lrowe.co.uk>
2025-05-05 20:15:55 -0700

    Add logging around program for debugging tinykvm

--- rust/tokio/src/main.rs
+++ rust/tokio/src/main.rs
@@ -8,17 +8,29 @@ mod varnish;
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), std::io::Error>  {
 	loop {
+		println!("Before: wait_for_requests_paused");
 		let _request = varnish::wait_for_requests_paused();
-		// python3 -m http.server
-		let mut stream = TcpStream::connect("127.0.0.1:8000").await?;
-		stream.write_all(
-			b"HEAD / HTTP/1.1\r\n\
-			Host: 127.0.0.1:8080\r\n\
-			Connection: close\r\n\
-			\r\n"
-		).await?;
+		println!("After: wait_for_requests_paused");
 		let mut buf = Vec::<u8>::new();
-		stream.read_to_end(&mut buf).await?;
+		{
+			// python3 -m http.server
+			println!("Before: TcpStream::connect(...).await?");
+			let mut stream = TcpStream::connect("127.0.0.1:8000").await?;
+			println!("After: TcpStream::connect(...).await?");
+			println!("Before: stream.write_all.await?");
+			stream.write_all(
+				b"HEAD / HTTP/1.1\r\n\
+				Host: 127.0.0.1:8080\r\n\
+				Connection: close\r\n\
+				\r\n"
+			).await?;
+			println!("After: stream.write_all.await?");
+			println!("Before: stream.read_to_end.await?");
+			stream.read_to_end(&mut buf).await?;
+			println!("After: stream.read_to_end.await?");
+		}
+		println!("Before: backend_response");
 		varnish::backend_response(200, "text/plain", &buf);
+		println!("After: backend_response");
 	}
 }
